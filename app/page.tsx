512019b2e79e31f573e43e54c5745a7160c7fca3
hmzsumon <zsumonn@gmail.com>
2025-03-08 16:12:50 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import Banner from '../components/Banner/Banner';
+import CoinsTable from '../components/CoinsTable';
+
+export default function Home() {
+	return (
+		<>
+			<Banner />
+			<CoinsTable />
+		</>
+	);
+}
